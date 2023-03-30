913df86455cc226658facd98d273e70c217fe3fa
Smital Kotkar <106620502+smitalkotkar@users.noreply.github.com>
2023-03-30 18:04:17 +0530

    Create HalfPyramid.java

---
+++ lab30-03-2023/HalfPyramid.java
@@ -0,0 +1,20 @@
+package basic;
+
+import java.util.*;
+
+public class HalfPyramid {
+	
+	public static void main(String args[]) 
+	{
+		 int rows = 7;
+
+	        for (int i = 1; i <= rows; i++) 
+	        {
+	            for (int j = 1; j <= i; j++) 
+	            {
+	                System.out.print("* ");
+	            }
+	            System.out.println();
+	        }
+	    }
+	}
